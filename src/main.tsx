e9cb92ffb0d3e03af8dba0d62951bc89e40302f1
mayconr4 <mayconhs2007@gmail.com>
2025-06-23 09:35:24 -0300

    dado descrições as linhas de código

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Ajuda a detectar uso de recursos obsoletos (APIS, funções, padrões etc )
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
+//Usado para criar a 'raiz' da palicação React a partir de um elmento HTML
+import { createRoot } from "react-dom/client";
+
+// Importa o componente principal da aplicação.
+// o componente App é o ponto de entrada da aplicação React
+import App from "./App.tsx";
+
+// Seleciona o elmento HTML root, cria a raiz da aplicação e renderiza o componente App.
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
