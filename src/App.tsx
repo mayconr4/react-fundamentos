1856fd494cb71e7ebfb4d8b203ea9d54bd7f4ce0
mayconr4 <mayconhs2007@gmail.com>
2025-06-23 10:03:28 -0300

    feito menu do react

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ export default function App() {
   // Comentário de uma linha
   /*Comentário de multiplas linhas */
   return (
-    <header>
-      <h1>olá React! 🪼</h1>
-      <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contatato</a>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>olá React! 🪼</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Sobre</a>
+          <a href="">Contatato</a>
+        </nav>
+      </header>
+      <main></main>
+    </>
   );
 }
