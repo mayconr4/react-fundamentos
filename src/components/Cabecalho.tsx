2a6111bd618389a139ccb4d9a6c30e57cfe3a6ee
mayconr4 <mayconhs2007@gmail.com>
2025-06-23 11:48:34 -0300

    alterando caminho da pasta

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,5 +1,5 @@
 /*Tudo o que for usado dentro de componentes (imagens, fontes etc ), deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
-import logo from "./assets/logo-react.png";
+import logo from "../assets/logo-react.png";
 
 export default function Cabecalho() {
   return (
