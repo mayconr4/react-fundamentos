dab516432f2e855dc7d1ad1376553711a7c4f44e
mayconr4 <mayconhs2007@gmail.com>
2025-06-23 11:01:56 -0300

    criado ufncao cabecallho

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
+import Cabecalho from "./Cabecalho";
+
 export default function App() {
   // Comentário de uma linha
   /*Comentário de multiplas linhas */
   return (
     <>
-      <header>
-        <h1>olá React! 🪼</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Sobre</a>
-          <a href="">Contatato</a>
-        </nav>
-      </header>
-
-      {/* Comentários no meio JSX: é normal ficar entre {} */}
-      {/* Ao usar novos elementos HTML  no JSX, é importante 
-      envolver tudo em <> </> (Fragmento React)  */}
+      <Cabecalho />
 
       <main>
         <section>
---
+++ src/Cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>olá React! 🪼</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Sobre</a>
+        <a href="">Contatato</a>
+      </nav>
+    </header>
+  );
+}
