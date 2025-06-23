e7501b572c7a7f3402657131d59e6d767757621e
mayconr4 <mayconhs2007@gmail.com>
2025-06-23 10:46:14 -0300

    exemplos de react

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Fundamentos de react
 
+## 01_usando-a-sintaxe-JSX-no-App
+
+- Construção de um componente único (App.tsx) usando elementos HTML e fragmento react
+
+- Referência sobre Componentes e JSX: https://react.dev/reference/react/Component
+- Instalação da exetnsão React Developer tools
+
 ## 00_projeto_zerado
 
 - Remoção do CSS
--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,54 @@ export default function App() {
           <a href="">Contatato</a>
         </nav>
       </header>
-      <main></main>
+
+      {/* Comentários no meio JSX: é normal ficar entre {} */}
+      {/* Ao usar novos elementos HTML  no JSX, é importante 
+      envolver tudo em <> </> (Fragmento React)  */}
+
+      <main>
+        <section>
+          <h2>Seja bem vindo(a) aos fundamentos de react</h2>
+          <p>Este é um exemplo de aplicação react</p>
+
+          <article>
+            <h3>Artigo 1 exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              culpa earum cupiditate minima quis id officia et quisquam illo sit
+              optio nihil deserunt accusamus, consectetur porro consequatur amet
+              temporibus vero?
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 2 exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              culpa earum cupiditate minima quis id officia et quisquam illo sit
+              optio nihil deserunt accusamus, consectetur porro consequatur amet
+              temporibus vero?
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 3 exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              culpa earum cupiditate minima quis id officia et quisquam illo sit
+              optio nihil deserunt accusamus, consectetur porro consequatur amet
+              temporibus vero?
+            </p>
+          </article>
+        </section>
+      </main>
+
+      <footer>
+        <h2>Pequeno site criado com react</h2>
+        <p>
+          Desenvolvido por <b>Maycon</b> &copy; 2025
+        </p>
+      </footer>
     </>
   );
 }
