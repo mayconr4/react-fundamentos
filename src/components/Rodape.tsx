1744fbe11d688b07e3f061d02fa87638d693fb4a
mayconr4 <mayconhs2007@gmail.com>
2025-06-24 10:01:32 -0300

    adicionando mais elementos css

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,9 +1,18 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
     <>
       <footer>
         <h2>Pequeno site criado com react</h2>
-        <p>
+        <p style={{ color: "red", fontWeight: "bold" }}>
           Desenvolvido por <b>Maycon</b> &copy; 2025
         </p>
       </footer>
