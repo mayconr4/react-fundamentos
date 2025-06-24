22a147af2ccc60ae61ace36eec2f4c16415d3f5c
mayconr4 <mayconhs2007@gmail.com>
2025-06-24 09:32:35 -0300

    cria e importa estilos global

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-// Ajuda a detectar uso de recursos obsoletos (APIS, funções, padrões etc )
 import { StrictMode } from "react";
-
-//Usado para criar a 'raiz' da palicação React a partir de um elmento HTML
 import { createRoot } from "react-dom/client";
-
-// Importa o componente principal da aplicação.
-// o componente App é o ponto de entrada da aplicação React
 import App from "./App.tsx";
 
-// Seleciona o elmento HTML root, cria a raiz da aplicação e renderiza o componente App.
+//Importando a folha de estilos CSS global
+import "./global.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
