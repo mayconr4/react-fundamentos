085098d311b3eb4d22992c9ea6067e619397a22a
mayconr4 <mayconhs2007@gmail.com>
2025-06-24 11:59:55 -0300

    em andamento

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,23 @@
+import styles from "./Menu.module.css";
+
+// export default function Menu() {
+//   return (
+//     <nav className={styles.menu}>
+//       <a  href="">Home</a>
+//       <a href="">Sobre</a>
+//       <a href="">Contatato</a>
+//     </nav>
+//   );
+// }
+
 export default function Menu() {
   return (
     <nav>
-      <a href="">Home</a>
-      <a href="">Sobre</a>
-      <a href="">Contatato</a>
+      <div className={styles.menuList}>
+        <div className={styles.menuItem}>Início</div>
+        <div className={styles.menuItem}>Sobre</div>
+        <div className={styles.menuItem}>Contato</div>
+      </div>
     </nav>
   );
 }
