176aa1bb5f48bc3f8b411295dce677e9cf9daa05
mayconr4 <mayconhs2007@gmail.com>
2025-06-24 09:45:39 -0300

    cria e aplica classe

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,10 +1,15 @@
 export default function Rodape() {
   return (
-    <footer>
-      <h2>Pequeno site criado com react</h2>
-      <p>
-        Desenvolvido por <b>Maycon</b> &copy; 2025
-      </p>
-    </footer>
+    <>
+      <footer>
+        <h2>Pequeno site criado com react</h2>
+        <p>
+          Desenvolvido por <b>Maycon</b> &copy; 2025
+        </p>
+      </footer>
+      <hr />
+      <p className="outro-texto"></p>
+      <hr />
+    </>
   );
 }
